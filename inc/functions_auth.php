e6f000004af207859273659d1886f532f9b75ddf
millerb08 <miller.steffens@gmail.com>
2020-04-15 17:56:06 -0500

    init

---
+++ inc/functions_auth.php
@@ -0,0 +1,60 @@
+<?php
+function isAuthenticated()
+{
+  global $session;
+  return $session->get('auth_logged_in', false);
+}
+
+function requireAuth()
+{
+  if (!isAuthenticated()) {
+    global $session;
+    $session->getFlashBag()->add('error', 'Not Authorized');
+    redirect('/login.php');
+  }
+}
+
+function isAdmin()
+{
+  if (!isAuthenticated()) {
+    return false;
+  }
+  
+  global $session;
+  return $session->get('auth_roles') === 1;
+}
+
+function requireAdmin()
+{
+  if (!isAdmin()) {
+    global $session;
+    $session->getFlashBag()->add('error', 'Not Authorized');
+    redirect('/login.php');
+  }
+}
+
+function isOwner($ownerId)
+{
+  if (!isAuthenticated()) {
+    return false;
+  }
+  
+  global $session;
+  return $ownerId == $session->get('auth_user_id');
+}
+
+function getAuthenticatedUser()
+{
+  global $session;
+  return findUserById($session->get('auth_user_id'));
+}
+
+function saveUserSession($user)
+{
+  global $session;
+  $session->set('auth_logged_in', true);
+  $session->set('auth_user_id', (int) $user['id']);
+  $session->set('auth_roles', (int) $user['role_id']);
+  
+  $session->getFlashBag()->add('success', 'Successfully Logged In');
+}
