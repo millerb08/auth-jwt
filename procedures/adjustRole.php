4689d7eaf8839f93993a1bd00335067a8b25a32d
millerb08 <miller.steffens@gmail.com>
2020-04-17 17:34:04 -0500

    fixed admin panel and roles

--- procedures/adjustRole.php
+++ procedures/adjustRole.php
@@ -1,11 +1,10 @@
 <?php
-require_once __DIR__ . '/../inc/bootstrap.php';
-requireAuth();
+require_once __DIR__."/../inc/bootstrap.php";
 
-$user = changeRole(request()->get('userId'), request()->get('roleId'));
-if ($user['role_id'] == 1) {
-  $session->getFlashBag()->add('success', $user['username'] . ' promoted to admin!');
-} else {
-  $session->getFlashBag()->add('success', $user['username'] . ' demoted to restricted user!');
+$user=changeRole(request()->get("userId"),request()->get("roleId"));
+if($user["role_id"] == 1){
+  $session->getFlashBag()->add("success", $user["username"]." promoted to admin!");
+}else{
+  $session->getFlashBag()->add("success", $user["username"]." demoted to restricted user!");
 }
-redirect('/admin.php');
+redirect("/admin.php");
