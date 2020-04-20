da7ea71a898d4e774667c812c660a6dbaf038f5e
millerb08 <miller.steffens@gmail.com>
2020-04-20 15:53:18 -0500

    add book and edit function also vote

--- procedures/addBook.php
+++ procedures/addBook.php
@@ -1,11 +1,12 @@
 <?php
-
 require_once __DIR__ .'/../inc/bootstrap.php';
+requireAuth();
 
 $bookTitle = request()->get('title');
 $bookDescription = request()->get('description');
+$ownerId = $session->get("auth_user_id");
 
-if (addBook($bookTitle, $bookDescription)) {
+if (addBook($bookTitle, $bookDescription, $ownerId)) {
     $session->getFlashBag()->add('success', 'Book Added');
     redirect('/books.php');
 } else {
