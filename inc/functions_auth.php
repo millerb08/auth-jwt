6978fbb989872f4ab14c4484de97eda222596861
millerb08 <miller.steffens@gmail.com>
2020-04-20 20:57:18 -0500

    remove cookie on logout

--- inc/functions_auth.php
+++ inc/functions_auth.php
@@ -43,7 +43,7 @@ function getAuthenticatedUser(){
   return findUserById($session->get("auth_user_id"));
 }
 
-function saveUserSession($user)
+function saveUserData($user)
 {
   global $session;
   $session->set('auth_logged_in', true);
@@ -51,4 +51,24 @@ function saveUserSession($user)
   $session->set('auth_roles', (int) $user['role_id']);
   
   $session->getFlashBag()->add('success', 'Successfully Logged In');
+  $data = [
+    "auth_user_id" => (int) $user['id'],
+    "auth_roles" => (int) $user['role_id']
+  ];
+  $expTime = time() + 3600; //3600 secods 1 hour
+  $cookie = setAuthCookie(json_encode($data),$expTime);
+  redirect("/", ["cookies" => [$cookie]]);
+}
+
+function setAuthCookie($data, $expTime){
+  $cookie = new Symfony\Component\HttpFoundation\Cookie(
+    "auth", //name
+    $data, //value
+    $expTime, //expiration time
+    "/",//the path for the cookie to be avaliable
+    ".treehouse-app.com",//Domain
+    false, //httpS
+    true //httpOnly
+  );
+  return $cookie;
 }
--- procedures/doLogout.php
+++ procedures/doLogout.php
@@ -1,8 +1,10 @@
 <?php
-require_once __DIR__."/../inc/bootstrap.php";
+require_once __DIR__ . '/../inc/bootstrap.php';
 
-$session->remove("auth_logged_in");
-$session->remove("auth_user_id");
-$session->remove("auth_roles");
-$session->getFlashBag()->add("success", "Successfully logged Out");
-redirect("/login.php");
+$session->remove('auth_logged_in');
+$session->remove('auth_user_id');
+$session->remove('auth_roles');
+
+$session->getFlashBag()->add('success', 'Successfully Logged Out');
+$cookie = setAuthCookie("expired", 1);
+redirect('/login.php', ["cookies" => [$cookie]]);
